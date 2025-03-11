f6d018862769f2e18d32c63eaf83766fd95e75e7
jwson-automation <sonjungwoo9@gmail.com>
2025-03-11 22:07:34 +0900

    추가 린트 수정 : main.dart에 누락분 수정하기

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,17 @@ import 'package:timer/ui/screens/timer_screen.dart';
 
 // void : 아무 것도 리턴하지 않는 함수
 void main() {
-  // main -> appStart!
-  runApp(ProviderScope(child: MyApp()));
+  // main -> appStart
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-// 위젯 : 플러터의 가장 기본이 되는 화면 구성 단위!
+/// 위젯 : 플러터의 가장 기본이 되는 화면 구성 단위
 class MyApp extends StatelessWidget {
-  // StatelessWidget : 상태가 없는 위젯
+  /// StatelessWidget : 상태가 없는 위젯
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // 짓고 있느냐!
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
