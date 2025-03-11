18d5ec85ea420a42b632833da357b8f4d07ff378
jwson-automation <sonjungwoo9@gmail.com>
2025-03-11 22:42:23 +0900

    로티애니메이션 위젯 분리

--- lib/ui/screens/timer_screen.dart
+++ lib/ui/screens/timer_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:lottie/lottie.dart';
 import 'package:timer/ui/providers/lottie_provider.dart';
 import 'package:timer/ui/providers/music_provider.dart';
 import 'package:timer/ui/providers/timer_provider.dart';
+import 'package:timer/ui/widgets/lottie_animation_widget.dart';
 
 /// TimerScreen : 뽀모도로 타이머 화면
 class TimerScreen extends ConsumerWidget {
@@ -77,43 +77,6 @@ class TimerScreen extends ConsumerWidget {
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-/// 로티 애니메이션 위젯
-class LottieAnimationWidget extends StatelessWidget {
-  /// 어떤 기준으로 로티 애니메이션을 보여줄지 확인하기 위한 값
-  final bool phase;
-
-  /// 애니메이션을 실행할지 말지 확인하기 위한 값
-  final bool animation;
-
-  /// 페이즈값이 true일 때 로티 이미지
-  final String beforeIamge;
-
-  /// 페이즈값이 false일 때 로티 이미지
-  final String afterImage;
-
-  /// 생성자
-  const LottieAnimationWidget({
-    super.key,
-    required this.phase,
-    required this.animation,
-    required this.beforeIamge,
-    required this.afterImage,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 200,
-      height: 200,
-      child: Lottie.asset(
-        phase ? beforeIamge : afterImage,
-        animate: animation,
-        fit: BoxFit.contain,
       ),
     );
   }
